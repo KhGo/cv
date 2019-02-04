c68f53da3504aba3b33acf479d706da8dca435cf
KhGo <46898691+KhGo@users.noreply.github.com>
2019-02-04 20:50:41 +0100

    Update script-cv.js

--- script-cv.js
+++ script-cv.js
@@ -4,16 +4,18 @@ let deplacement  = (window.scrollY*100)/(document.body.offsetHeight-window.inner
     if (document.querySelector('#progress_bar').MaterialSlider){
         document.querySelector('#progress_bar').MaterialSlider.change(deplacement);
     }
-});
-
-
-
-function myScroll() { // Définition de la fonction de scroll
-    var progress = document.getElementById('progress_bar').value; // Valeur de la barre de progression
     
-    let progress = document.getElementById('progress_bar').value; 
+function myScroll() { 
+    var progress = document.getElementById('progress_bar').value;
+    
+   let value =  progress;
     
     // Calculez ici la valeur de déplacement à efectuer lors du scroll
     
     window.scroll(0, value) // Déplacement de la page de 0 px latéralement et 'value' px horizontalement
 }
+});
+
+
+
+
